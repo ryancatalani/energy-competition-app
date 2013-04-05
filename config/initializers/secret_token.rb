c365da53cd25a4a2c42d164b2a486a5252b6338d
Ryan Catalani <catalani.ryan@gmail.com>
2013-04-05 00:17:03 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnergyCompetitionApp::Application.config.secret_token = 'ad365e25f79ab452510bd0cfc040cdbf75e2651386c926d7a7385adc5774d4782a5f8d04e0d85ae946168a9a1c0472dfa812c017476675c6c8f5bb303bc2ea84'
