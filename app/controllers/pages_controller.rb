be02ecd0e320c1578083dfe3c4e369bac1c1063e
Ryan Catalani <catalani.ryan@gmail.com>
2013-04-08 00:40:43 -0400

    Use kWh instead of just W. Because of the shower time metric, there'd be no good way to compare plain watts, and energy is measured in kWh, anyway.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,8 @@ class PagesController < ApplicationController
   	avg_device_wattage = 80
   	avg_shower_wattage = 20000
   	avg_roommates = 3.5
+  	avg_hours_lights_on = 5.9 # Assuming an daily average (over a week) of 7 hours of sleep, 2.2 hours of class, and spending half the time of the room.
+  	avg_hours_devices_in = 19 # Most devices stay plugged in (or not plugged in)
 
   	@wattage = {}
   	@recycled_items = {}
@@ -21,41 +23,43 @@ class PagesController < ApplicationController
   	@recycled_items[:lb_latest] = @recycled_items[:lb].last[1]
 
   	@wattage[:prow] = prow.map do |day_entries|
-  		avg_shower_time = day_entries.sum(&:shower_time) / day_entries.count
-  		this_avg_shower_wattage = avg_shower_time * avg_shower_wattage
+  		avg_shower_time = day_entries.sum(&:shower_time) / day_entries.count # In minutes
+  		this_avg_shower_wh = (avg_shower_time / 60) * avg_shower_wattage
 
   		avg_devices = day_entries.sum(&:plugged_in) / day_entries.count
-  		this_avg_devices_wattage = avg_devices * avg_device_wattage
+  		this_avg_devices_wh = avg_devices * avg_device_wattage * avg_hours_devices_in
 
   		avg_personal_lights = day_entries.sum(&:personal_lightbulbs) / day_entries.count
-  		this_avg_personal_light_wattage = avg_personal_lights * avg_lightbulb_wattage
+  		this_avg_personal_light_wh = avg_personal_lights * avg_lightbulb_wattage * avg_hours_lights_on
 
   		rooms = day_entries.group_by{|e| e.room}.values.map {|r| r.sum(&:shared_lightbulbs) / r.count }
   		avg_shared_lights = rooms.reduce(:+) / rooms.count
-  		this_avg_shared_lights_wattage = (avg_shared_lights / avg_roommates) * avg_lightbulb_wattage
+  		this_avg_shared_lights_wh = (avg_shared_lights / avg_roommates) * avg_lightbulb_wattage * avg_hours_lights_on
 
-  		sum = this_avg_shower_wattage + this_avg_devices_wattage + this_avg_personal_light_wattage + this_avg_shared_lights_wattage
+  		sum_wh = this_avg_shower_wh + this_avg_devices_wh + this_avg_personal_light_wh + this_avg_shared_lights_wh
+  		sum_kwh = sum_wh / 1000.0
 
-  		[day_entries.first.created_at.midnight.to_i * 1000, sum]
+  		[day_entries.first.created_at.midnight.to_i * 1000, sum_kwh]
   	end
 
   	@wattage[:lb] = lb.map do |day_entries|
-  		avg_shower_time = day_entries.sum(&:shower_time) / day_entries.count
-  		this_avg_shower_wattage = avg_shower_time * avg_shower_wattage
+  		avg_shower_time = day_entries.sum(&:shower_time) / day_entries.count # In minutes
+  		this_avg_shower_wh = (avg_shower_time / 60) * avg_shower_wattage
 
   		avg_devices = day_entries.sum(&:plugged_in) / day_entries.count
-  		this_avg_devices_wattage = avg_devices * avg_device_wattage
+  		this_avg_devices_wh = avg_devices * avg_device_wattage * avg_hours_devices_in
 
   		avg_personal_lights = day_entries.sum(&:personal_lightbulbs) / day_entries.count
-  		this_avg_personal_light_wattage = avg_personal_lights * avg_lightbulb_wattage
+  		this_avg_personal_light_wh = avg_personal_lights * avg_lightbulb_wattage * avg_hours_lights_on
 
   		rooms = day_entries.group_by{|e| e.room}.values.map {|r| r.sum(&:shared_lightbulbs) / r.count }
   		avg_shared_lights = rooms.reduce(:+) / rooms.count
-  		this_avg_shared_lights_wattage = (avg_shared_lights / avg_roommates) * avg_lightbulb_wattage
+  		this_avg_shared_lights_wh = (avg_shared_lights / avg_roommates) * avg_lightbulb_wattage * avg_hours_lights_on
 
-  		sum = this_avg_shower_wattage + this_avg_devices_wattage + this_avg_personal_light_wattage + this_avg_shared_lights_wattage
+  		sum_wh = this_avg_shower_wh + this_avg_devices_wh + this_avg_personal_light_wh + this_avg_shared_lights_wh
+  		sum_kwh = sum_wh / 1000.0
 
-  		[day_entries.first.created_at.midnight.to_i * 1000, sum]
+  		[day_entries.first.created_at.midnight.to_i * 1000, sum_kwh]
   	end
 
   	@wattage[:prow_latest] = @wattage[:prow].last[1]
